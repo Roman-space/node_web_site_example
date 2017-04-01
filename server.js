51e7b2b59a944e7d74141423754d74a4d1a23692
Rom <rom@Book.local>
2017-04-01 11:49:09 +0300

    basic of Express

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    //res.send('Hello Express!');
+    res.send({
+        name: 'Roman',
+        likes: [
+            'Sea'
+        ]
+    });
 });
 
+app.get('/about',(req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fullfill the page'
+    });
+});
 app.listen(3000);
