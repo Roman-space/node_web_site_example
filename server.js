a63b6de6172fbe174a0b976dfe6662d342226fa9
Rom <rom@Book.local>
2017-04-01 23:48:49 +0300

    add hablebars {

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-// absolute path to the folder 
-// __dirname = stores path to  server folder
+    // it is how to enable htb with express 
+    // for templates - default dir views = time to create
+    // file must have extentions  hbs
+    // to rendr file using view engine 
+    //  --- response.render('about.hbs || file_name');
+app.set('view engine', 'hbs');
 
-//set the root path for static documents.
+    // absolute path to the folder 
+    // __dirname = stores path to  server folder
+
+    //set the root path for static documents.
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('Hello Express!');
-    res.send({
-        name: 'Roman',
-        likes: [
-            'Sea'
-        ]
+    /*
+        res.send({
+            name: 'Roman',
+            likes: [
+                'Sea'
+            ]
+        });
+    */
+    res.render('w.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Hi, live is really great'
     });
 });
 
 app.get('/about',(req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    }); 
 });
 
 app.get('/bad', (req, res) => {
@@ -27,4 +45,6 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to fullfill the page'
     });
 });
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
